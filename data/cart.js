daa8784694bf41a55d312c87a30ff26c54b3f7a6
Adam Whitehouse <adswhitehouse95@gmail.com>
2025-02-10 14:59:06 +0000

    Cart uses local storage

--- data/cart.js
+++ data/cart.js
@@ -1,14 +1,11 @@
 // 5 - Create cart array that will contain product data of the items added to the cart
-export let cart = [
-  {
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 2
-  },
-  {
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 1
-  }
-];
+
+export let cart = JSON.parse(localStorage.getItem("cart")) || []
+
+function saveToStorage() {
+  localStorage.setItem("cart", JSON.stringify(cart))
+}
+
 //
 export function addToCart(productId) {
   const quantitySelection = document.querySelector(
@@ -31,6 +28,7 @@ export function addToCart(productId) {
       quantity: Number(quantitySelection),
     });
   }
+  saveToStorage()
 }
 
 export function removeItemFromCart(productId) {
@@ -41,4 +39,5 @@ export function removeItemFromCart(productId) {
     }
   })
   cart = newCart
+  saveToStorage()
 }
